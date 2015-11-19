3d794bbc280f63db31c9642592bd140003886542
david-schopf <david.schopf@gmail.com>
2015-11-19 17:26:05 +0100

    Solution :)

--- hw4.c
+++ hw4.c
@@ -9,23 +9,32 @@ int matrix[10][10];
 bool isDiagonal() {
     int row = 0;
     int column = 0;
+    int value;
 
     for (row = 0; row < size; row ++) {
         for (column = 0; column < size; column ++) {
-
+            value = matrix[row][column];
+            // Check every cell if its off-diagonal and not 0
+            if (row != column && value != 0 )
+                return false;
         }
     }
-
+    // No error until here: Matrix is diagonal!
     return true;
 }
 
 bool isSymmetric() {
     int row = 0;
     int column = 0;
+    int value;
 
     for (row = 0; row < size; row ++) {
         for (column = 0; column < size; column ++) {
-
+            value = matrix[row][column];
+            int transposedValue =  matrix[column][row];
+            // Check every cell if its transposed cell has the same value
+            if (value != transposedValue)
+                return false;
         }
     }
 
@@ -35,10 +44,15 @@ bool isSymmetric() {
 bool isSkewSymmetric() {
     int row = 0;
     int column = 0;
+    int value;
 
     for (row = 0; row < size; row ++) {
         for (column = 0; column < size; column ++) {
-
+            value = matrix[row][column];
+            int transposedValue =  matrix[column][row];
+            // Check every cell if its transposed cell has the corresponding negative value
+            if (value != (-1)*transposedValue)
+                return false;
         }
     }
 
@@ -51,7 +65,7 @@ int main() {
     // Size of the matrix
     scanf("%d", &size);
 
-    if (size < 1 || size > 10) {
+    if (!size || size < 1 || size > 10) {
         printf("INVALID MATRIX SIZE\n");
         // Error
         return -1;
@@ -70,12 +84,16 @@ int main() {
 
     printf("RESULT\n");
 
-    if (isDiagonal())
+    if (isDiagonal()) {
         printf("The matrix is diagonal\n");
-    if (isSymmetric())
-    printf("The matrix is symmetric\n");
-    if (isSkewSymmetric())
-    printf("The matrix is skew - symmetric\n");
+    }
+    if (isSymmetric()) {
+        printf("The matrix is symmetric\n");
+    }
+
+    if (isSkewSymmetric()) {
+        printf("The matrix is skew - symmetric\n");
+    }
 
     // Success
     return 0;
